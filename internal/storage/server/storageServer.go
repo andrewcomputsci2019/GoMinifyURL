df4c47a8af81e61de90787c9026fb989522fade6
Andrew Pegg <apegg@ucsd.edu>
2025-06-18 16:26:08 -0700

    add import

---
+++ internal/storage/server/storageServer.go
@@ -0,0 +1,27 @@
+package server
+
+import (
+	"GOMinifyURL/internal/proto"
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type StorageServer struct {
+	db *pgxpool.Pool
+	proto.UnimplementedURLStorageServer
+}
+
+func (s StorageServer) PutURL(ctx context.Context, request *proto.ShortURLRequest) (*proto.ShortUrlResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s StorageServer) GetFullURL(ctx context.Context, request *proto.FullUrlRequest) (*proto.FullUrlResponse, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s StorageServer) mustEmbedUnimplementedURLStorageServer() {
+	//TODO implement me
+	panic("implement me")
+}
