872a3fc04854a8f4233ff35d4e100edc3bf0e674
Andrew Pegg <apegg@ucsd.edu>
2025-10-17 17:02:48 -0700

    http test added and passing

--- internal/admin/adminServer_test.go
+++ internal/admin/adminServer_test.go
@@ -1,22 +1,23 @@
 package admin
 
 import (
+	"GOMinifyURL/internal/api/admin"
+	"GOMinifyURL/internal/middleware/logger"
 	"GOMinifyURL/internal/proto"
 	"context"
+	"encoding/json"
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
-
-var httpServer *http.Server
-
-var httpAddress string
 
 func getFreePort() (string, error) {
 	for i := 0; i < 5; i++ {
@@ -29,6 +30,15 @@ func getFreePort() (string, error) {
 		return addr, nil
 	}
 	return "", fmt.Errorf("unable to find free port after retries")
+}
+
+func TestMain(m *testing.M) {
+	err := os.Setenv("TESTING_ENV", "true")
+	viper.MustBindEnv("TESTING_ENV")
+	if err != nil {
+		return
+	}
+	os.Exit(m.Run())
 }
 
 func TestNewGrpcAdminServer(t *testing.T) {
@@ -342,3 +352,305 @@ func TestGrpcAdminServer_RequestServiceList(t *testing.T) {
 }
 
 // todo admin http server test
+
+func addServiceHelper(t *testing.T, grpcServer *GrpcAdminServer, message *proto.RegistrationMessage) {
+	t.Helper()
+	cxt, cFun := context.WithDeadline(context.Background(), time.Now().Add(time.Second*5))
+	_, err := grpcServer.RegisterService(cxt, message)
+	cFun()
+	if err != nil {
+		t.Errorf("GRPC rpc call of RegisterService failed: %v", err)
+		return
+	}
+}
+
+func createGrpcAdminServerAndServe(t *testing.T) *GrpcAdminServer {
+	t.Helper()
+	addr, err := getFreePort()
+	if err != nil {
+		t.Errorf("GetFreePort() error = %v", err)
+		return nil
+	}
+	adminServer, err := NewGrpcAdminServer(addr, WithLeaseTimes(time.Second*60))
+	if err != nil {
+		t.Errorf("NewGrpcAdminServer() error = %v", err)
+		return nil
+	}
+	go func() {
+		_ = adminServer.ListenAndServe()
+	}()
+	return adminServer
+}
+
+func TestNewHTTPAdminServer(t *testing.T) {
+	test, err := NewGrpcAdminServer("localhost:7070")
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+	_, err = NewHTTPAdminServer("localhost:8080", test)
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+}
+
+func TestHttpAdminServer_StartAndListenAndClose(t *testing.T) {
+	grpcServer := createGrpcAdminServerAndServe(t)
+	if grpcServer == nil {
+		t.Errorf("GrpcAdminServer is nil")
+		return
+	}
+	defer grpcServer.Close()
+	addr, err := getFreePort()
+	if err != nil {
+		t.Errorf("GetFreePort() error = %v", err)
+		return
+	}
+	defer grpcServer.Close()
+	httpServer, err := NewHTTPAdminServer(addr, grpcServer, WithMiddleWares([]MiddlewareFunc{logger.LoggingMiddleware()}))
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+	defer func(httpServer *HTTPAdminServer) {
+		err := httpServer.Close()
+		if err != nil {
+			t.Errorf("http server close() error = %v", err)
+		}
+	}(httpServer)
+	go func() {
+		_ = httpServer.StartAndListen()
+	}()
+	time.Sleep(150 * time.Millisecond)
+	res, err := http.Get("http://" + addr + "/api_admin/getServices")
+	if err != nil {
+		t.Errorf("Http Server may not be serving content")
+		return
+	}
+	err = res.Body.Close()
+	if err != nil {
+		return
+	}
+}
+
+func TestHTTPAdminServer_GetServices(t *testing.T) {
+	t.Parallel()
+	grpcServer := createGrpcAdminServerAndServe(t)
+	if grpcServer == nil {
+		t.Errorf("GrpcAdminServer is nil")
+		return
+	}
+	defer grpcServer.Close()
+	addr, err := getFreePort()
+	if err != nil {
+		t.Errorf("GetFreePort() error = %v", err)
+		return
+	}
+	defer grpcServer.Close()
+	httpServer, err := NewHTTPAdminServer(addr, grpcServer, WithMiddleWares([]MiddlewareFunc{logger.LoggingMiddleware()}))
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+	defer httpServer.Close()
+	go func() {
+		_ = httpServer.StartAndListen()
+	}()
+	time.Sleep(150 * time.Millisecond)
+	addServiceHelper(t, grpcServer, &proto.RegistrationMessage{
+		ServiceName:  "test-service",
+		InstanceName: "1",
+		DialAddr:     "just-a-test",
+	})
+	if t.Failed() {
+		return
+	}
+	resp, err := http.Get("http://" + addr + "/api_admin/getServices")
+	if err != nil {
+		t.Errorf("Http Server may not be serving content")
+		return
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("Error fetching content from getServices endpoint: status code %v", resp.StatusCode)
+		return
+	}
+	var jsonData []admin.Service
+	err = json.NewDecoder(resp.Body).Decode(&jsonData)
+	if err != nil {
+		t.Errorf("Error decoding response from getServices endpoint")
+		return
+	}
+	if len(jsonData) != 1 {
+		t.Errorf("Expected 1 service, got %d in json response %v", len(jsonData), jsonData)
+	}
+	if jsonData[0].Id != "1" || jsonData[0].Name != "test-service" || jsonData[0].Url != "just-a-test" {
+		t.Errorf("Data sent by server in json repsonse does not match service def {ID: 1, Name: 'test-service', URL: 'just-a-test': got %v", jsonData[0])
+		return
+	}
+}
+
+func TestHTTPAdminServer_GetServiceList(t *testing.T) {
+	t.Parallel()
+	grpcServer := createGrpcAdminServerAndServe(t)
+	if grpcServer == nil {
+		t.Errorf("GrpcAdminServer is nil")
+		return
+	}
+	defer grpcServer.Close()
+	addr, err := getFreePort()
+	if err != nil {
+		t.Errorf("GetFreePort() error = %v", err)
+		return
+	}
+	defer grpcServer.Close()
+	httpServer, err := NewHTTPAdminServer(addr, grpcServer, WithMiddleWares([]MiddlewareFunc{logger.LoggingMiddleware()}))
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+	defer httpServer.Close()
+	go func() {
+		_ = httpServer.StartAndListen()
+	}()
+	time.Sleep(150 * time.Millisecond)
+	addServiceHelper(t, grpcServer, &proto.RegistrationMessage{ServiceName: "test-service", InstanceName: "1", DialAddr: "just-a-test"})
+	if t.Failed() {
+		return
+	}
+	addServiceHelper(t, grpcServer, &proto.RegistrationMessage{ServiceName: "not-test-service", InstanceName: "2", DialAddr: "just-a-test"})
+	if t.Failed() {
+		return
+	}
+	resp, err := http.Get("http://" + addr + "/api_admin/getServiceList/test-service")
+	if err != nil {
+		t.Errorf("Http Server may not be serving content")
+		return
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("Error fetching content from getServiceList endpoint: status code %v", resp.StatusCode)
+		return
+	}
+	var jsonData []admin.Service
+	err = json.NewDecoder(resp.Body).Decode(&jsonData)
+	if err != nil {
+		t.Errorf("Error decoding response from getServiceList endpoint")
+		return
+	}
+	if len(jsonData) != 1 {
+		t.Errorf("Expected 1 service, got %d in json response %v", len(jsonData), jsonData)
+	}
+	if jsonData[0].Id != "1" {
+		t.Errorf("Expected service id '1', got %v", jsonData[0])
+		return
+	}
+	if jsonData[0].Name != "test-service" {
+		t.Errorf("Expected service name 'test-service', got %v", jsonData[0])
+		return
+	}
+}
+
+func TestHTTPAdminServer_GetServiceInfo(t *testing.T) {
+	t.Parallel()
+	grpcServer := createGrpcAdminServerAndServe(t)
+	if grpcServer == nil {
+		t.Errorf("GrpcAdminServer is nil")
+		return
+	}
+	defer grpcServer.Close()
+	addr, err := getFreePort()
+	if err != nil {
+		t.Errorf("GetFreePort() error = %v", err)
+		return
+	}
+	defer grpcServer.Close()
+	httpServer, err := NewHTTPAdminServer(addr, grpcServer, WithMiddleWares([]MiddlewareFunc{logger.LoggingMiddleware()}))
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+	defer httpServer.Close()
+	go func() {
+		_ = httpServer.StartAndListen()
+	}()
+	time.Sleep(150 * time.Millisecond)
+	addServiceHelper(t, grpcServer, &proto.RegistrationMessage{
+		ServiceName:  "test-service",
+		InstanceName: "1",
+		DialAddr:     "just-a-test",
+	})
+	if t.Failed() {
+		return
+	}
+	serviceINFO, err := grpcServer.leaseManager.GetServiceInfo("1")
+	if err != nil {
+		t.Errorf("GetServiceInfo() error = %v", err)
+		return
+	}
+	resp, err := http.Get("http://" + addr + "/api_admin/serviceInfo?id=1")
+	if err != nil {
+		t.Errorf("Http Server may not be serving content")
+		return
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("Error fetching content from getServiceInfo endpoint: status code %v", resp.StatusCode)
+		return
+	}
+	var jsonData admin.ServiceHealth
+	err = json.NewDecoder(resp.Body).Decode(&jsonData)
+	if err != nil {
+		t.Errorf("Error decoding response from getServiceInfo endpoint: %v", err)
+		return
+	}
+	if jsonData.Service.Name != "test-service" || jsonData.Service.Id != "1" || !jsonData.LeaseInformation.Equal(serviceINFO.lease.leaseTime) {
+		t.Errorf("data returned by getServiceInfo does not match service info expcted %v but got %v", serviceINFO, jsonData)
+		return
+	}
+}
+
+func TestHTTPAdminServer_RemoveService(t *testing.T) {
+	t.Parallel()
+	grpcServer := createGrpcAdminServerAndServe(t)
+	if grpcServer == nil {
+		t.Errorf("GrpcAdminServer is nil")
+		return
+	}
+	defer grpcServer.Close()
+	addr, err := getFreePort()
+	if err != nil {
+		t.Errorf("GetFreePort() error = %v", err)
+		return
+	}
+	defer grpcServer.Close()
+	httpServer, err := NewHTTPAdminServer(addr, grpcServer, WithMiddleWares([]MiddlewareFunc{logger.LoggingMiddleware()}))
+	if err != nil {
+		t.Errorf("NewHTTPAdminServer() error = %v", err)
+	}
+	defer httpServer.Close()
+	go func() {
+		_ = httpServer.StartAndListen()
+	}()
+	time.Sleep(150 * time.Millisecond)
+	addServiceHelper(t, grpcServer, &proto.RegistrationMessage{
+		ServiceName:  "test-service",
+		InstanceName: "1",
+		DialAddr:     "just-a-test",
+	})
+	if t.Failed() {
+		return
+	}
+	req, err := http.NewRequest(http.MethodDelete, "http://"+addr+"/api_admin/removeService?id=1", nil)
+	if err != nil {
+		t.Errorf("Could not construct request: %v", err)
+		return
+	}
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Errorf("Http Server may not be serving content: %v", err)
+		return
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("Error remvoing content from removeService endpoint: status code %v", resp.StatusCode)
+		return
+	}
+	// make sure item is gone
+	_, err = grpcServer.leaseManager.GetServiceInfo("1")
+	if err == nil {
+		t.Errorf("GetServiceInfo() should have failed when removing a service that should not exist")
+		return
+	}
+}
